ef920b7523d21bb865c15711595ef9f303c16cb7
Pascal Vorwerk <408670@student.saxion.nl>
2024-05-05 10:37:07 +0200

    removed PUUID , fix in circleCI :)

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,7 @@
 import type { Config } from "@jest/types";
 
 process.env.RIOT_LOL_API_KEY = "661771";
-process.env.PUUID =
-  "JjKdGcffExfOCsfimlsP2QOnoXA-lZCJL9jM2KeLkmEw6UGxi1ZguLAEaCs_eOY3zJeyaZO1KmcMDQ";
+process.env.PUUID = "";
 const config: Config.InitialOptions = {
   verbose: true,
   coverageDirectory: ".jest/coverage",
